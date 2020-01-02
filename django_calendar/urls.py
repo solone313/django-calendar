447a037afb5bdd44cedcd691e02943586bee6ffa
solone313 <solone313@naver.com>
2020-01-02 15:44:29 +0900

    add url update setting

--- django_calendar/urls.py
+++ django_calendar/urls.py
@@ -6,5 +6,6 @@ app_name = 'django_calendar'
 urlpatterns = [
     path('monthly/', MakeMonthly.as_view(),),
     path('monthly/<int:year>/<int:month>/<int:day>/', MakeMonthly.as_view(), name='monthly'),
+    path('schedule_data/', ScheduleListView.as_view(), name='schedule_data'),
 
 ] 
--- myapp/settings/base.py
+++ myapp/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'myapp.home',
     'myapp.user',
     'django_calendar'
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'layout')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
